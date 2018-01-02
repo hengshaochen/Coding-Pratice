da543133057734bcf7da8327e1735a20919b158b
hengshaochen <henrychen0702@gmail.com>
2018-01-02 01:38:18 -0500

    1.2

--- Binary Tree Longest Consecutive Sequence II.java
+++ Binary Tree Longest Consecutive Sequence II.java
@@ -1,51 +1,75 @@
 // 正解
-class ResultType {
-    public int max_length, max_down, max_up;
-    ResultType(int len, int down, int up) {
-        max_length = len;
-        max_down = down;
-        max_up = up;    
-    }
-}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
 
 public class Solution {
-    /**
-     * @param root the root of binary tree
-     * @return the length of the longest consecutive sequence path
+    /*
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
      */
-    public int longestConsecutive2(TreeNode root) {
-        // Write your code here
-        return helper(root).max_length;
-    }
-    
-    ResultType helper(TreeNode root) {
-        if (root == null) {
-            return new ResultType(0, 0, 0);
+    class result {
+        int up = 0;
+        int down = 0;
+        result (int up, int down) {
+            this.up = up;
+            this.down = down;
         }
-
-        ResultType left = helper(root.left);
-        ResultType right = helper(root.right);
-
-        int down = 0, up = 0;
-        if (root.left != null && root.left.val + 1 == root.val)
-            down = Math.max(down, left.max_down + 1);
-
-        if (root.left != null && root.left.val - 1 == root.val)
-            up = Math.max(up, left.max_up + 1);
-
-        if (root.right != null && root.right.val + 1 == root.val)
-            down = Math.max(down, right.max_down + 1);
-
-        if (root.right != null && root.right.val - 1 == root.val)
-            up = Math.max(up, right.max_up + 1);
-
-        int len = down + 1 + up;
-        len = Math.max(len, Math.max(left.max_length, right.max_length));
-
-        return new ResultType(len, down, up);
+    }
+    int ans = 0;
+    public int longestConsecutive2(TreeNode root) {
+        // down為從下往上遞增, up為從上往下遞增
+        // 定義up為從下往上 小到大 / down為從上往下 大到小的連續序列長度
+        // 1. 分派任務給兒子, 左右兒子要給我up, down, max_len
+        // 2. (a) 透過我跟左右兒子的數值關係來更新up, down. 把當前root.val和兒子比較, 若連續,則針對up或down增加 若不連續, 則把up down歸0
+        //    (b) 更新max_len --> up 和 down 組合再一起 + 1 <-- root本身
+        // 左右子樹都會回傳up / down, 都選Max的
+        // 3. 回傳max_len, up, down給父親
+        dfs(root);
+        return ans;
+    }
+    result dfs(TreeNode root) {
+        if (root == null) {
+            return new result(0, 0);
+        }
+        int up = 0;
+        int down = 0;
+        
+        // Step1
+        result left = dfs(root.left);
+        result right = dfs(root.right);
+        
+        // Step2
+        // a
+        if (root.left != null && root.left.val + 1 == root.val) {
+            down = Math.max(down, left.down + 1);
+        }
+        if (root.left != null && root.left.val - 1 == root.val) {
+            up = Math.max(up, left.up + 1);
+        }
+        if (root.right != null && root.right.val + 1 == root.val) {
+            down = Math.max(down, right.down + 1);
+        }
+        if (root.right != null && root.right.val - 1 == root.val) {
+            up = Math.max(up, right.up + 1);
+        }
+        // b
+        int cur_len = down + 1 + up;
+        if (ans < cur_len) {
+            ans = cur_len;
+        }
+        
+        // Step3
+        return new result(up, down);
     }
 }
-
 
 
 // 錯誤 有bug {2,#,3,2,#,1} 會出錯, 因為沒有分開判斷up, down
